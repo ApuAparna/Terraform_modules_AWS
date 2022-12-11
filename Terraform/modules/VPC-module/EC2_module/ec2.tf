401ab6d26e3d4223a2ed4122c6b4543eaa221ffa
ApuAparna <103950097+ApuAparna@users.noreply.github.com>
2022-12-11 09:11:26 +0530

    upload_ec2_module

---
+++ Terraform/modules/VPC-module/EC2_module/ec2.tf
@@ -0,0 +1,8 @@
+module "VPC" {
+  source = "../modules/vpc"
+}
+module "EC2" {
+  source = "../modules/ec2"
+  EC2_Subnet_ID ="${module.VPC.public_subnet_name}"
+  vpc_id = "${module.VPC.vpc_name}"
+}
